9020184aac8135e207cfc56541af3877088724e3
Ossitech <ossitech@googlemail.com>
2021-03-14 03:22:44 +0100

    added comments

--- main.cpp
+++ main.cpp
@@ -4,17 +4,28 @@
 
 #include "InfInt.h"
 
+/*
+Fakultätsfunktion
+*/
 InfInt fac(InfInt n){
     if (n == 0) return 1;
     return n * fac(n - 1);
 }
 
+/*
+Funktion "n über k" oder Binomialkoeffizient
+*/
 InfInt over(InfInt n, InfInt k){
     return fac(n) / (fac(k) * fac(n - k));
 }
-
+/*
+Enthält am Ende die Ergebnisse
+*/
 std::map<int, InfInt> m;
 
+/*
+Die eigentliche Fubinifunktion
+*/
 InfInt f(int n){
     if (n == 0){
             m[n] = 1;
@@ -29,11 +40,15 @@ InfInt f(int n){
 }
 
 int main(int argc, char** argv){
+    //Standardmäßig wird für n 0 gewählt.
     int n = 0;
 
+    //Übergabeparameter verwenden
     if(argc > 1) n = std::atoi(argv[1]);
 
     f(n);
+
+    //Ausgabe der Liste
 
     std::map<int, InfInt>::iterator it;
     int i = 0;
